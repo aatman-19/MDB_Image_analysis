9b82e6edea6365cc5f2bc0365395f780a774b667
aatmanrp <aatmanrp@gmail.com>
2023-10-08 17:03:19 -0700

    color code feature working properly

--- Algo.py
+++ Algo.py
@@ -9,9 +9,41 @@ for im in os.listdir("test_images"):
     num_img += 1
 
 
+class Algo:
+    def __init__(self):
+        self.cc_feature_matrix = [[0] * 64] * num_img
+        self.ic_feature_matrix = [[0] * 26] * num_img
+        self.num_images = num_img
+
+
+def color_code_histogram():
+    img_folder = "test_images"
+    feature_mat = np.zeros(num_img * 65).reshape(num_img, 65)
+    count = 0
+    for cur_img in os.listdir(img_folder):
+        image = cv2.imread(os.path.join(img_folder, cur_img))
+        histogram = np.zeros(65, dtype=np.int32)
+        histogram[0] = image.size
+        for row in image:
+            for pixel in row:
+                r, g, b = pixel[2], pixel[1], pixel[0]  # Extract R, G, and B values
+
+                # Extract the two most significant bits from each channel
+                r_bits = (r >> 6) & 0b11
+                g_bits = (g >> 6) & 0b11
+                b_bits = (b >> 6) & 0b11
+
+                # Concatenate the bits into a decimal value (RGB format)
+                concat_value = (r_bits << 4) | (g_bits << 2) | b_bits
+                histogram[concat_value + 1] += 1
+        feature_mat[count] = histogram
+        count += 1
+    return feature_mat
+
+
 def intensity_code_histogram():
     img_folder = "test_images"
-    feature_mat = np.zeros(78).reshape(num_img, 26)
+    feature_mat = np.zeros(num_img * 26).reshape(num_img, 26)
     count = 0
     for cur_img in os.listdir(img_folder):
         image = cv2.imread(os.path.join(img_folder, cur_img))
@@ -30,21 +62,24 @@ def intensity_code_histogram():
 
 
 def manhattan_dist(selected_img, other_img):
-    sum = 0
+    d = 0
     for i in range(1, 26):
-        sum = +abs((selected_img[i] / selected_img[0]) - (other_img[i] / other_img[0]))
-    print(sum)
+        d += abs((selected_img[i] / selected_img[0]) - (other_img[i] / other_img[0]))
+    return d
 
 
-class Algo:
-    def __init__(self):
-        self.cc_feature_matrix = [[0] * 64] * num_img
-        self.ic_feature_matrix = [[0] * 26] * num_img
+def get_distance_vector(selected_img):
+    dist_vector = np.zeros(num_img, dtype=np.float64)
+    for i in range(0, num_img):
+        dist_vector[i] = (manhattan_dist(selected_img, f_mat[i]))
+    return dist_vector
 
 
-f_mat = intensity_code_histogram()
+# testing functions for INTENSITY
+f_mat = color_code_histogram()
 np.set_printoptions(suppress=True)
-print(f_mat)
-manhattan_dist(f_mat[0], f_mat[0])
-manhattan_dist(f_mat[0], f_mat[1])
-manhattan_dist(f_mat[0], f_mat[2])
+print(np.array2string(f_mat, separator=', '))
+
+print("\n >now getting distance vector")
+dv = get_distance_vector(f_mat[1])
+print(np.array2string(dv, separator=', '))
